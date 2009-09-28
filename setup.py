86072f65777761a055b9cb790966d0005ba6dbe5
Thorsten Weimann <thorsten.weimann@gmx.net>
2009-09-28 21:20:43 +0200

    Changed version in setup.py to 0.2.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 
 setup(
     name='pyBarcode',
-    version='0.2',
+    version='0.2.1',
     packages=['barcode', 'barcode.writer'],
     url='http://trac-hg.assembla.com/pybarcode',
     #download_url='',
@@ -16,7 +16,7 @@ setup(
     author='Thorsten Weimann',
     author_email='thorsten.weimann@gmx.net',
     description=('Create standard barcodes with Python and save them as SVG. '
-                 'No external module needed.'),
+                 'No external modules needed.'),
     long_description=getdoc(barcode),
     classifiers=[
         'Development Status :: 4 - Beta',
