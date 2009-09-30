b6f97c75601eac7fc09716703f4229c044845d38
Thorsten Weimann <thorsten.weimann@gmx.net>
2009-09-30 10:33:57 +0200

    SVG creation is now using xml.dom instead of string formatting. Text not working yet.

--- barcode/writer/svg.py
+++ barcode/writer/svg.py
@@ -1,62 +1,127 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
 """barcode.writer.svg
 
 """
 __docformat__ = 'restructuredtext en'
 
+import xml.dom
+
 from writerbase import BaseWriter
 
 
 # SVG stuff
-DOCUMENT = u"""<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
-    "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+#DOCUMENT = u"""<?xml version="1.0" encoding="UTF-8"?>
+#<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
+#    "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+#
+#<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+#%s
+#</svg>
+#"""
+#MODULE = (u'<rect x="%.3fmm" y="%.3fmm" width="%.3fmm" height="%.3fmm" '
+#          u'style="fill:%s;" />')
+#TEXT = (u'<text x="%.3fmm" y="%.3fmm" style="fill:%s;font-size:%dpt;'
+#        u'text-anchor:middle;">%s</text>')
 
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-%s
-</svg>
-"""
-MODULE = (u'<rect x="%.3fmm" y="%.3fmm" width="%.3fmm" height="%.3fmm" '
-          u'style="fill:%s;" />')
-TEXT = (u'<text x="%.3fmm" y="%.3fmm" style="fill:%s;font-size:%dpt;'
-        u'text-anchor:middle;">%s</text>')
+SIZE = '{0:.3f}mm'
+
+
+def _set_attributes(element, **attributes):
+    for key, value in attributes.items():
+        element.setAttribute(key, value)
+
+
+def create_svg_object():
+    imp = xml.dom.getDOMImplementation()
+    doctype = imp.createDocumentType(
+        'svg',
+        '-//W3C//DTD SVG 1.1//EN',
+        'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'
+    )
+    document = imp.createDocument('http://www.w3.org/2000/svg', 'svg', doctype)
+    _set_attributes(document.documentElement, version='1.1',
+                    xmlns='http://www.w3.org/2000/svg')
+    return document
 
 
 class SVGWriter(BaseWriter):
 
     def __init__(self, **options):
-        self.module_width = options.get('module_width', 10)
-        self.module_height = options.get('module_height', 10)
-        self.font_size = options.get('font_size', 10)
-        self.quiet_zone = options.get('quiet_zone', 6.5)
-        self.background = options.get('background', u'white')
-        self.foreground = options.get('foreground', u'black')
-        self.text = options.get('text', u'')
-        self._document = DOCUMENT
+        BaseWriter.__init__(self)
+        self.set_options(**options)
+        #self._document = DOCUMENT
+        self._document = create_svg_object()
+
+    def _create_bar_element(self, xpos, ypos, width, color):
+        element = self._document.createElement('rect')
+        attributes = dict(x=SIZE.format(xpos), y=SIZE.format(ypos),
+                          width=SIZE.format(width),
+                          height=SIZE.format(self.module_height),
+                          style='fill:{0};'.format(color))
+        _set_attributes(element, **attributes)
+        return element
+
+    def _create_text_element(self, xpos, ypos):
+        element = self._document.createElement('text')
+        element.data = self.text
+        attributes = dict(x=SIZE.format(xpos), y=SIZE.format(ypos),
+                          style='fill:{0};font-size:{1}pt;text-anchor:'
+                                'middle;'.format(self.foreground,
+                                                 self.font_size))
+        _set_attributes(element, **attributes)
+        return element
 
     def render(self, code):
-        lines = []
+        root = self._document.documentElement
         ypos = 1.0
         for line in code:
             # Left quiet zone is x startposition
             xpos = self.quiet_zone
             for mod in line:
-                if mod == u'0':
+                if mod == '0':
                     color = self.background
                 else:
                     color = self.foreground
-                lines.append(MODULE % (xpos, ypos, self.module_width,
-                                       self.module_height, color))
+                new_child = self._create_bar_element(xpos, ypos,
+                                                     self.module_width, color)
+                root.appendChild(new_child)
                 xpos += self.module_width
             # Add right quiet zone to every line
-            lines.append(MODULE % (xpos, ypos, self.quiet_zone,
-                                   self.module_height, self.background))
+            new_child = self._create_bar_element(xpos, ypos, self.quiet_zone,
+                                                 self.background)
+            root.appendChild(new_child)
             ypos += self.module_height
         if self.text:
             ypos += self.font_size / 3.54 + 1
             xpos = xpos / 2.0
-            lines.append(TEXT % (xpos, ypos, self.foreground, self.font_size,
-                                 self.text))
-        return self._document % (u'\n'.join(lines), )
+            root.appendChild(self._create_text_element(xpos, ypos))
+        return self._document.toprettyxml(encoding='UTF-8')
+
+#    def render(self, code):
+#        lines = []
+#        ypos = 1.0
+#        for line in code:
+#            # Left quiet zone is x startposition
+#            xpos = self.quiet_zone
+#            for mod in line:
+#                if mod == u'0':
+#                    color = self.background
+#                else:
+#                    color = self.foreground
+#                lines.append(MODULE % (xpos, ypos, self.module_width,
+#                                       self.module_height, color))
+#                xpos += self.module_width
+#            # Add right quiet zone to every line
+#            lines.append(MODULE % (xpos, ypos, self.quiet_zone,
+#                                   self.module_height, self.background))
+#            ypos += self.module_height
+#        if self.text:
+#            ypos += self.font_size / 3.54 + 1
+#            xpos = xpos / 2.0
+#            lines.append(TEXT % (xpos, ypos, self.foreground, self.font_size,
+#                                 self.text))
+#        return self._document % (u'\n'.join(lines), )
 
--- barcode/writer/writerbase.py
+++ barcode/writer/writerbase.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import unicode_literals
+
 """barcode.writer.writerbase
 
 """
@@ -7,12 +9,39 @@ __docformat__ = 'restructuredtext en'
 
 
 class BaseWriter(object):
+    """Baseclass for all writers."""
+
+    def __init__(self):
+        """Initializes the basic writer options. Childclasses can add more
+        attributes and can set them directly or using
+        `self.set_options(option=value)`.
+        """
+        self.module_width = 10
+        self.module_height = 10
+        self.font_size = 10
+        self.quiet_zone = 6.5
+        self.background = 'white'
+        self.foreground = 'black'
+        self.text = ''
 
     def set_options(self, **options):
+        """Sets the given keyword arguments as instance attributes (only if
+        they are known).
+
+        :parameters:
+            options : Keyword arguments
+                All known instance attributes and more if the childclass has
+                defined them before this call.
+
+        :rtype: None
+        """
         for key, val in options.items():
             key = key.lstrip('_')
             if hasattr(self, key):
                 setattr(self, key, val)
 
     def render(self, code):
+        """Renders the barcode to whatever the inheriting writer provides.
+        Childclasses must implement this.
+        """
         raise NotImplementedError
