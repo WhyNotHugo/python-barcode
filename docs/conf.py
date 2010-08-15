3ec42fc5a9d9e0bdeecc2cf25dd9b9e5103202e9
Thorsten Weimann <thorsten.weimann@gmx.net>
2010-08-15 17:18:19 +0200

    Added logo to docs (selfcreated barcode).

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,7 @@ import sys, os
 #sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('..'))
 import barcode
+LOGO = os.path.join('images', 'pybarcode.png')
 
 # -- General configuration -----------------------------------------------------
 
@@ -112,7 +113,7 @@ html_theme = 'default'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = LOGO
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
