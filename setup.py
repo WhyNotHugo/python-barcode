c71d1633c37780944be669910f09e5c4445fe5a3
Hugo Osvaldo Barrera <hugo@barrera.io>
2018-07-26 16:56:26 -0300

    Officially support Python 3.7

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Multimedia :: Graphics',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
