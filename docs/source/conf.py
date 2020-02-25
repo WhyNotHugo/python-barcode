5121d3ddd7bf94bf6cc2e462405708393d7e6b8b
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-02-25 01:14:04 +0100

    Change the rfd theme to a more modern one

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,14 +94,18 @@ pygments_style = 'sphinx'
 
 # -- Options for HTML output ---------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  Major themes that come with
-# Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'default'
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'github_user': 'WhyNotHugo',
+    'github_repo': 'python-barcode',
+    'github_banner': 'true',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
