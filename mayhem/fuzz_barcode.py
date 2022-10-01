92eb5bb5df88d32239affccc81d7bf96f837271b
jaketbrown <jaketbrown1119@gmail.com>
2022-10-01 10:39:06 -0400

    updates

--- mayhem/fuzz_barcode.py
+++ mayhem/fuzz_barcode.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
 import sys
 import atheris
 import os
 from io import BytesIO
 import barcode
 from barcode.writer import SVGWriter
+from barcode.errors import BarcodeError
 
 @atheris.instrument_func
 def TestOneInput(data):
@@ -14,21 +14,19 @@ def TestOneInput(data):
     
     barray=bytearray(data)
     fdp=atheris.FuzzedDataProvider(data)
-    result = fdp.ConsumeInt(len(data)) + 1100001000011
-  
-    EAN = barcode.get_barcode_class('ean13')
+    result = fdp.ConsumeInt(len(data))
     try:
+      EAN = barcode.get_barcode_class('ean13')
       my_ean = EAN(str(result))
       fullname = my_ean.save('test_barcode')
-      with open("somefile.svg", "wb") as f:
-        EAN(str(result), writer=SVGWriter()).write(f)
-    except Exception:
-      None
+    except BarcodeError:
+      None  
+    
     
   
 
 def main():
-  #atheris.instrument_all()
+  atheris.instrument_all()
   atheris.Setup(sys.argv, TestOneInput, enable_python_coverage=True)
   atheris.Fuzz()
 
