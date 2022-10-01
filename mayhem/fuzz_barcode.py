c8b17ed63afa47e4a55c03684280a80b3e413289
jaketbrown <jaketbrown1119@gmail.com>
2022-10-01 01:59:15 -0400

    update to docker file install right module, update fuzzer script

--- mayhem/fuzz_barcode.py
+++ mayhem/fuzz_barcode.py
@@ -3,20 +3,28 @@ import atheris
 import os
 from io import BytesIO
 import barcode
-from barcode import EAN13
-#from barcode.writer import SVGWriter
+from barcode.writer import SVGWriter
+
 @atheris.instrument_func
 def TestOneInput(data):
-  barray=bytearray(data)
-  fdp=atheris.FuzzedDataProvider(data)
-  try:
+
+    if len(data) < 1:
+      return
+    
+    barray=bytearray(data)
+    fdp=atheris.FuzzedDataProvider(data)
+    result = fdp.ConsumeInt(len(data)) + 1100001000011
+  
     EAN = barcode.get_barcode_class('ean13')
-    my_ean = EAN(fdp.ConsumeString(data))
-    fullname = my_ean.save('test_barcode')
-  except Exception:
-    None
-
-
+    try:
+      my_ean = EAN(str(result))
+      fullname = my_ean.save('test_barcode')
+      with open("somefile.svg", "wb") as f:
+        EAN(str(result), writer=SVGWriter()).write(f)
+    except Exception:
+      None
+    
+  
 
 def main():
   atheris.instrument_all()
