81563369fd66c7485ea1c3d244f0a3f2276c75af
Aarni Koskela <akx@iki.fi>
2023-11-28 13:06:03 +0200

    Mark as officially supported on Python 3.12

--- README.rst
+++ README.rst
@@ -26,7 +26,7 @@ python-barcode
 There are no external dependencies when generating SVG files.
 Pillow is required for generating images (e.g.: PNGs).
 
-Support Python 3.8 to 3.11.
+Support Python 3.8 to 3.12.
 
 .. image:: example-ean13.png
   :target: https://github.com/WhyNotHugo/python-barcode
--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Multimedia :: Graphics",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
