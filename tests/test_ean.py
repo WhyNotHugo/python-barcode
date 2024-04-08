bb2175ae43ff5549e73e891c9f8acb41d97466e4
Hugo Osvaldo Barrera <hugo@whynothugo.nl>
2024-04-08 15:55:43 +0200

    Improve test for EAN13

--- tests/test_ean.py
+++ tests/test_ean.py
@@ -3,9 +3,19 @@ from __future__ import annotations
 from barcode.ean import EAN13
 
 
-def test_ean_checksum() -> None:
-    ean = EAN13("842169142322")  # input has 12 digits
-    assert ean.calculate_checksum() == 0
+def test_ean_checksum_generated() -> None:
+    ean = EAN13("842167143322")  # input has 12 digits
+    assert ean.calculate_checksum() == 5
+    assert ean.ean == "8421671433225"
 
-    ean = EAN13("8421691423220")  # input has 13 digits
-    assert ean.calculate_checksum() == 0
+
+def test_ean_checksum_zeroed() -> None:
+    ean = EAN13("842167143322", no_checksum=True)  # input has 12 digits
+    assert ean.calculate_checksum() == 5
+    assert ean.ean == "8421671433220"
+
+
+def test_ean_checksum_supplied_and_generated() -> None:
+    ean = EAN13("8421671433225")  # input has 13 digits
+    assert ean.calculate_checksum() == 5
+    assert ean.ean == "8421671433225"
